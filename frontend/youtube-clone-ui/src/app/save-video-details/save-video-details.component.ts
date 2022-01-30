e336ae38017a3f11e11f063b89cdb6362900be24
Young wook Jeon <ela87@naver.com>
2022-01-31 00:48:01 +0900

    Save metadata of a video

--- frontend/youtube-clone-ui/src/app/save-video-details/save-video-details.component.ts
+++ frontend/youtube-clone-ui/src/app/save-video-details/save-video-details.component.ts
@@ -5,6 +5,7 @@ import {COMMA, ENTER} from "@angular/cdk/keycodes";
 import {ActivatedRoute} from "@angular/router";
 import {VideoService} from "../video.service";
 import {MatSnackBar} from "@angular/material/snack-bar";
+import {VideoDto} from "../video-dto";
 
 @Component({
   selector: 'app-save-video-details',
@@ -27,6 +28,7 @@ export class SaveVideoDetailsComponent implements OnInit {
   videoId = '';
   fileSelected = false;
   videoUrl!: string;
+  thumbnailUrl!: string;
 
   constructor(private activatedRoute: ActivatedRoute,
               private videoService: VideoService,
@@ -34,6 +36,7 @@ export class SaveVideoDetailsComponent implements OnInit {
     this.videoId = this.activatedRoute.snapshot.params.videoId;
     this.videoService.getVideo(this.videoId).subscribe(data => {
       this.videoUrl = data.videoUrl;
+      this.thumbnailUrl = data.thumbnailUrl;
     });
     this.saveVideoDetailsForm = new FormGroup({
       title: this.title,
@@ -81,5 +84,21 @@ export class SaveVideoDetailsComponent implements OnInit {
         console.log(data);
         this.matSnackBar.open("Thumbnail Upload Successful", "OK");
       });
+  }
+
+  saveVideo() {
+    const videoMetaData: VideoDto = {
+      "id": this.videoId,
+      "title": this.saveVideoDetailsForm.get("title")?.value,
+      "description": this.saveVideoDetailsForm.get("description")?.value,
+      "tags": this.tags,
+      "videoStatus": this.saveVideoDetailsForm.get("videoStatus")?.value,
+      "videoUrl": this.videoUrl,
+      "thumbnailUrl": this.thumbnailUrl
+    }
+
+    this.videoService.saveVideo(videoMetaData).subscribe(data => {
+      this.matSnackBar.open("Video Metadata Updated successfully", "OK");
+    });
   }
 }
