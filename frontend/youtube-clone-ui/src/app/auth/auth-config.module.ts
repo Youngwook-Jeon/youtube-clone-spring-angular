5850db256036406f377cc3d0b6bfa30788b8566a
Young wook Jeon <ela87@naver.com>
2022-02-04 13:55:16 +0900

    Fix a bugs associated with routes

--- frontend/youtube-clone-ui/src/app/auth/auth-config.module.ts
+++ frontend/youtube-clone-ui/src/app/auth/auth-config.module.ts
@@ -7,17 +7,16 @@ import {AuthModule, LogLevel} from 'angular-auth-oidc-client';
         config: {
             authority: 'https://dev-fcad9kyt.us.auth0.com',
             redirectUrl: window.location.origin,
-            postLogoutRedirectUri: window.location.origin,
             clientId: 'FD6wDjK4RGs4s9Nvza0msH54TQJUjbC3',
             scope: 'openid profile offline_access',
             responseType: 'code',
             silentRenew: true,
             useRefreshToken: true,
             logLevel: LogLevel.Debug,
-            // secureRoutes: ["http://localhost:8080/"],
-            // customParamsAuthRequest: {
-            //   audience: "http://localhost:8080/"
-            // }
+            secureRoutes: ["http://localhost:8080/"],
+            customParamsAuthRequest: {
+              audience: 'http://localhost:8080'
+            }
         }
       })],
     exports: [AuthModule],
